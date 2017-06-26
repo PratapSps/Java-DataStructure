c55e8a326f500fb3bfee90b9d9ed6b2b6296da55
survya <singh.survya@gmail.com>
2017-06-26 16:39:06 -0400

    BST

--- src/sps/BST.java
+++ src/sps/BST.java
@@ -1,6 +1,19 @@
 package sps;
 
 public class BST {
+	
+	int data;
+	BST leftNode;
+	BST rightnode;
+	
+	public BST(int item){
+		data=item;
+		
+	}
+	
+	public void insert(int data){
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
